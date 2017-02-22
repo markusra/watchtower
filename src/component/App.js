b5b51c572c350c512a1d39cd8ae0de1e577ee277
Håkon Edøy Hanssen <Hkon Edy Hanssen>
2017-02-22 11:12:45 +0100

    Added container to main page

--- src/component/App.js
+++ src/component/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from "./Button.js"
+import ContentContainer from "./Container.js"
+import styleContainer from "./Container.js"
 
+const style = {
+  textAlign: 'center',
+  padding: '10',
+  borderRadius: '10',
+  width: '70%',
+  border: '3px solid black'
+}
+
+function Container(props) {
+  return (
+     <div className="container" style={style}>
+       {props.children}
+     </div>
+   );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="mainContainer">
-          WatchTower
-          <input type="text" name="searchInput" className="searchInput" placeholder="Søk"/>
-          <button>Fra</button>
-          <button>Til</button>
-          <Button
-            buttonText = "Søk"
-            />
+          <Container>
+            WatchTower
+            <input type="text" name="searchInput" className="searchInput" placeholder="Søk"/>
+            <button>Fra</button>
+            <button>Til</button>
+            <Button
+              buttonText = "Søk"
+              />
+            </Container>
         </div>
       </div>
     );
---
+++ src/component/Container.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+const ContentContainer = () => (
+  <div className="containerDiv" style={styles.container}>
+  </div>
+);
+
+export default ContentContainer;
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '10',
+    borderRadius: '10',
+    width: '70%',
+    border: '1px solid black'
+  }
+
+}
